15a8224057ae84476e865a2d91063eeeec2de2fc
unknown <wolihi@gmail.com>
2024-12-19 12:44:25 +0800

    change deploy api addr

--- src/config/baseConfig.ts
+++ src/config/baseConfig.ts
@@ -1,3 +1,3 @@
-export const baseURL_dev: string = 'http://127.0.0.1:17788/api/v1/'
-export const baseURL_pro: string = 'http://127.0.0.1:17788/api/v1/'
-export const customPort: number = 3001
+export const baseURL_dev: string = 'https://novelsapi.sanaoll.com/api/v1/'
+export const baseURL_pro: string = 'https://novelsapi.sanaoll.com/api/v1/'
+export const customPort: number = 27788
