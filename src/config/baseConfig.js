1863a13df32f0747513694ad205ed450832930c0
unknown <wolihi@gmail.com>
2025-01-07 23:28:26 +0800

    Final commit for cn6000 project

--- src/config/baseConfig.js
+++
@@ -1,6 +0,0 @@
-export const baseURL_dev = process.env.BASE_URL_DEV || 'http://127.0.0.1:17788/api/v1/';
-export const baseURL_pro = process.env.BASE_URL_PRO || 'https://novelsapi.sanaoll.com/api/v1/';
-export const customPort = Number(process.env.CUSTOM_PORT) || 27788;
-console.log('Base URL (Dev):', baseURL_dev);
-console.log('Base URL (Pro):', baseURL_pro);
-console.log('Custom Port:', customPort);
--- src/config/baseConfig.ts
+++ src/config/baseConfig.ts
@@ -2,6 +2,9 @@ export const baseURL_dev: string = process.env.BASE_URL_DEV || 'http://127.0.0.1
 export const baseURL_pro: string = process.env.BASE_URL_PRO || 'https://novelsapi.sanaoll.com/api/v1/';
 export const customPort: number = Number(process.env.CUSTOM_PORT) || 27788;
 
-console.log('Base URL (Dev):', baseURL_dev);
-console.log('Base URL (Pro):', baseURL_pro);
+
+export const baseURL: string = process.env.NODE_ENV === 'production' ? baseURL_pro : baseURL_dev;
+
+console.log('Current Environment:', process.env.NODE_ENV);
+console.log('Base URL:', baseURL);
 console.log('Custom Port:', customPort);
