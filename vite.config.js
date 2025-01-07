55c4d20b74fb8df7c7dafff4a41b1f1e9be7e390
unknown <wolihi@gmail.com>
2025-01-07 23:01:13 +0800

    Final commit for cn6000 project

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { customPort } from './src/config/baseConfig';
+import * as dotenv from 'dotenv';
+dotenv.config();
+// https://vitejs.dev/config/
+export default defineConfig({
+    define: {
+        'process.env': {
+            BASE_URL_DEV: process.env.BASE_URL_DEV,
+            BASE_URL_PRO: process.env.BASE_URL_PRO,
+            CUSTOM_PORT: process.env.CUSTOM_PORT,
+        }
+    },
+    server: {
+        port: customPort,
+        open: true
+    },
+    plugins: [
+        vue(),
+        createSvgIconsPlugin({
+            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+            symbolId: 'icon-[dir]-[name]'
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
+});
